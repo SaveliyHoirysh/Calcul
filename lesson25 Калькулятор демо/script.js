96f78f0ef83d9c89835cfdacab73f03ef77654c5
SaveliyHoirysh <98319435+SaveliyHoirysh@users.noreply.github.com>
2023-05-20 14:23:26 +0500

    123

---
+++ lesson25 Калькулятор демо/script.js
@@ -0,0 +1,7 @@
+/*let input = document.querySelector("#input");
+let btn = document.querySelector("#btn");
+
+btn.addEventListener("click", function () {
+    let result = eval(input.value);
+    console.log(result);
+})*/
